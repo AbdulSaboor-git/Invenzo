eb0caaa35e605b47838c4885b62de8ffe02ffa09
AbdulSaboor-git <abdulsaboor11524@gmail.com>
2024-08-14 16:40:05 +0500

    fs

--- src/app/home/inventory/[inventoryId]/components/add_product.js
+++ src/app/home/inventory/[inventoryId]/components/add_product.js
@@ -222,7 +222,7 @@ export default function AddProduct({
               value={tags}
               onChange={handleTagsChange}
               className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-600"
-              maxLength={100}
+              maxLength={400}
             />
           </div>
 
