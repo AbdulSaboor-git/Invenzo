1b23eb5e80c56f33dfed735a297350dadfa8dfb5
AbdulSaboor <abdulsaboor11524@gmail.com>
2025-02-13 17:31:39 +0500

    feat: implement service worker for offline caching and enhance theme management

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,27 +9,37 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   useEffect(() => {
     const theme = JSON.parse(localStorage.getItem("theme"));
-    const scheme = localStorage.getItem("colorScheme"); 
+    const scheme = localStorage.getItem("colorScheme");
     if (theme !== null) {
       document.documentElement.setAttribute(
         "data-theme",
         theme ? "light" : "dark"
       );
-    }
-    else{
-      document.documentElement.setAttribute(
-        "data-theme",
-        "light"
-      );
-    localStorage.setItem("theme", true);
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", true);
     }
     if (scheme !== null) {
       document.documentElement.setAttribute("color-scheme", scheme);
-    }
-    else{
+    } else {
       document.documentElement.setAttribute("color-scheme", "green");
-    localStorage.setItem("colorScheme", "green");
+      localStorage.setItem("colorScheme", "green");
+    }
+  }, []);
 
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((registration) => {
+          console.log(
+            "Service Worker registered with scope:",
+            registration.scope
+          );
+        })
+        .catch((error) => {
+          console.error("Service Worker registration failed:", error);
+        });
     }
   }, []);
 
