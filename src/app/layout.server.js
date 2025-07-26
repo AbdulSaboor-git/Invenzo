e67a4caa57d215c014fcd1137e4d65b1a98c06f3
AbdulSaboor <abdulsaboor11524@gmail.com>
2025-07-26 14:16:09 +0500

    refactor: remove unused metadata export and reorganize layout files

--- src/app/layout.server.js
+++ src/app/layout.server.js
@@ -3,6 +3,6 @@ export const metadata = {
   title: "Invenzo - Inventory Management",
   description: "",
   icons: {
-    icon: "/icons/invenzo-128.ico",
+    icon: "/public/icons/invenzo-512.ico",
   },
 };
