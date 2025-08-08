5ac7f67434ecf122695a4c9e4cd66692c1eaa331
AbdulSaboor <abdulsaboor11524@gmail.com>
2025-08-09 03:30:51 +0500

    fix: move localStorage operations into useEffect for proper execution after login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -48,10 +48,12 @@ export default function Login() {
 
       // Handle successful login (e.g., store token, user info, redirect user)
       // Set the cookies
-      if (typeof window !== "undefined") {
-        localStorage.setItem("token", data.token);
-        localStorage.setItem("user", JSON.stringify(data.user));
-      }
+      useEffect(() => {
+        if (typeof window !== "undefined") {
+          localStorage.setItem("token", data.token);
+          localStorage.setItem("user", JSON.stringify(data.user));
+        }
+      });
       toast.success("Logged in successfully");
       dispatch(setUser(data.user)); // Store user in Redux
       router.push("/inventory"); // Redirect to a different page after successful login
